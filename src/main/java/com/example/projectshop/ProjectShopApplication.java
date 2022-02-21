bd2575aedb8e5e3597d041e8d1c866a00260a784
Dejan Markoski <74320371+DejanMarkoski@users.noreply.github.com>
2022-02-21 19:16:33 +0100

    Update ProjectShopApplication.java

--- src/main/java/com/example/projectshop/ProjectShopApplication.java
+++ src/main/java/com/example/projectshop/ProjectShopApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ProjectShopApplication {
 
     public static void main(String[] args) {
+        System.out.println("HELLO");
         SpringApplication.run(ProjectShopApplication.class, args);
     }
 
